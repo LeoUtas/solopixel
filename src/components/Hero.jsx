5006e411fd78fbd5e22acac4bf3b09ca168650d3
LeoUTas <hoang.nguyen0@utas.edu.au>
2025-03-19 23:30:34 -0230

    revised minor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,24 +9,20 @@ const Hero = () => {
 
     return (
         <Section
-            className="pt-[12rem] -mt-[5.25rem] mb-[20vh]"
+            className="pt-[12rem] -mt-[5.25rem]"
             crosses
             crossesOffset="lg:translate-y-[5.25rem]"
             customPaddings
             id="hero"
         >
-            <div className="container relative" ref={parallaxRef}>
+            <div className="container relative mt-[10vh]" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
-                    <h1 className="h1 mb-6">
-                        Explore the Possibilities of&nbsp;AI&nbsp;Chatting with{" "}
-                        {` `}
-                        <span className="inline-block relative">
-                            Brainwave{" "}
-                        </span>
-                    </h1>
+                    <h1 className="h1 mb-6">Hey! Welcome to</h1>
+                    <h1 className="h1 mb-6">Solo Pixel Universe</h1>
                     <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-                        Unleash the power of AI within Brainwave. Upgrade your
-                        productivity with Brainwave, the open AI chat app.
+                        A place where you can unleash the power of AI within
+                        Brainwave. Upgrade your productivity with Brainwave, the
+                        open AI chat app.
                     </p>
                 </div>
 
